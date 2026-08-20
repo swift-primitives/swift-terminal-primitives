ff934f5d0ad56dfad79f9f125faf9a66dc3293f1
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:09:01 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-terminal-primitives",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         // MARK: - Namespace
@@ -31,7 +31,10 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-error-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-error-primitives.git",
+            branch: "main"
+        )
     ],
     targets: [
         // MARK: - Namespace
@@ -69,7 +72,7 @@ let package = Package(
         .target(
             name: "Terminal Primitives Test Support",
             dependencies: [
-                "Terminal Primitives",
+                "Terminal Primitives"
             ],
             path: "Tests/Support"
         ),
